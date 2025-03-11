353134417367038468f815c327b27b6f83f8633c
manas_pachauri <manaspachauri31@gmail.com>
2025-03-11 16:55:17 +0530

    Fixed Auth0 with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,22 @@ import CoinContextProvider from './context/CoinContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>  {/* Use HashRouter instead of BrowserRouter */}
+    <HashRouter>
       <CoinContextProvider>
         <Auth0Provider
           domain="dev-pv7d0sprxrlcgd1d.us.auth0.com"
           clientId="ytxIUWnobsoiA9qeouylTcwr864e70cR"
           authorizationParams={{
-            redirect_uri: `${window.location.origin}/#/` // Fix Auth0 redirect issue
+            redirect_uri: `${window.location.origin}/#/`
           }}
+          onRedirectCallback={(appState) => console.log("🔄 Auth0 Redirect Callback Triggered:", appState)}
         >
+          {console.log("✅ Auth0Provider Loaded!")}
           <App />
         </Auth0Provider>
       </CoinContextProvider>
     </HashRouter>
-  </StrictMode>
+  </StrictMode>,
 );
+
 
